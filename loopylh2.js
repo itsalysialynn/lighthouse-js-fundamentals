9511bab6770b13bb8f372f1a2c0d0b30c1c85767
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-06 17:53:59 +0000

    made spacing changes

--- loopylh2.js
+++ loopylh2.js
@@ -1,5 +1,6 @@
-function loopyLighthouse(range,multiples,words) {
+function loopyLighthouse (range,multiples,words) {
   var i = range[0];
+
   while (i >= range[0] && i <= range[1]) {
     if ( i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log(words[0] + words[1]);
